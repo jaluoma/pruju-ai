e6dd44dfd25233f75e957d840fd5f0115fc806a4
Jukka Luoma <jukka.luoma@aalto.fi>
2023-10-12 19:16:53 +0300

    Added theming

---
+++ aaltotheming.py
@@ -0,0 +1,23 @@
+from gradio.themes.utils.colors import Color
+from gradio.themes import GoogleFont
+from gradio.themes import Soft
+
+aaltoblue = Color(
+    name="aaltoblue",
+    c50= "#ECF6FF",
+    c100= "#D4E9FF",
+    c200= "#A9D0FF",
+    c300= "#7EB7FF",
+    c400= "#539EFF",
+    c500= "#46A5FF", # Aalto Blue
+    c600= "#3A8FE6",
+    c700= "#2F79CD",
+    c800= "#2653B4",
+    c900= "#1D2D9B",
+    c950= "#141784",
+    )
+
+aaltobluetheme = Soft(
+    primary_hue="aaltoblue",secondary_hue="aaltoblue",
+    font=[GoogleFont('Inter'), GoogleFont('Besley')],
+)
--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import gradio as gr
 from chat_caller import query_gpt_chat
 import os
 
+from aaltotheming import aaltobluetheme
+
 def courseGPT(query, chat_history):
     max_tokens = 2000
     # unpack history
@@ -19,11 +21,11 @@ with open(os.getenv("CHAT_DATA_FOLDER")+'/examples_ui.txt', 'r') as file:
 
 examples = [example.strip() for example in examples]
 
-gradio_app = gr.ChatInterface(fn=courseGPT, analytics_enabled=False, examples = examples)
+demo = gr.ChatInterface(fn=courseGPT, analytics_enabled=False, examples = examples,theme=aaltobluetheme)
 
 if __name__ == "__main__":
     print("Launching Demo\n")
     server_name = "127.0.0.1"
     isDocker = os.path.exists("/.dockerenv")
     print(f"Docker: {isDocker}\n")
-    gradio_app.launch(server_name="0.0.0.0" if isDocker else "127.0.0.1", root_path="/coursegpt")
+    demo.launch(server_name="0.0.0.0" if isDocker else "127.0.0.1", root_path="/coursegpt")
