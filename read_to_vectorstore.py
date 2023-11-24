31a90750a8ab0c2db9c6ba612655dff936bb2a24
Jukka Luoma <jukka.luoma@aalto.fi>
2023-11-24 15:06:21 +0200

    Refactor read_to_vectorstore.py

--- read_to_vectorstore.py
+++ read_to_vectorstore.py
@@ -1,48 +1,23 @@
 import argparse
+import os
+import textract
+import pandas as pd
+import os 
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
 
-def main():
-
-    parser=argparse.ArgumentParser(description="Read course materials and save to vector store.")
-    parser.add_argument('-d','--load_dir',help="Directory for the course materials. ",required=False, default="course_material")
-    parser.add_argument('-s','--save_dir',help="Directory to save the vector store.",required=False, default="course_material_vdb")
-    parser.add_argument('-u','--use_defaults',action='store_true', help="Run the script with sensible defaults.",required=False)
-    args = parser.parse_args()
-
-    import os 
-    import textract
-    import pandas as pd
-    from langchain.embeddings import HuggingFaceInstructEmbeddings
-    from langchain.vectorstores import FAISS
-
-    # Scan files from the course_material directory (or alternative)
-    material_directory=args.load_dir+"/"
-    folder = args.save_dir+"/"
-    use_defaults = args.use_defaults
-    print("Running script with the following arguments:")
-    print(f"Directory for the course materials: {material_directory}")
-    print(f"Directory to save the vector store: {folder}")
-    print(f"Run the script with sensible defaults: {use_defaults}")
-    print("---")
-
-    if use_defaults:
-        dir_input = material_directory
-    else:
-        dir_input = input(f"Default materials directory: {material_directory}\nPress enter to keep the same, or write the desired name to change: ")
-    if dir_input != '':
-        material_directory = dir_input
+def read_filenames_from_directory(material_directory: str):
     filenames = []
     for root, dirs, files in os.walk(material_directory):
         for name in files:
             # Exclude dot-files
             if name[0] != '.':
                 filenames.append(os.path.join(root, name))
+    return filenames
 
-    print("The following files will be processed:\n")
-    print(filenames)
-
+def create_material_headings_from_filenames(filenames, material_directory):
     # Make headings pretty based on file names
     # '_' to ' ', remove file suffixes, title case, "/" to ": " 
-    
     material_headings = [filename[len(material_directory):] for filename in filenames]
     def pretty_headings(heading):
         heading = heading.replace('_', ' ')
@@ -51,20 +26,9 @@ def main():
         heading = heading.replace('/', ': ') 
         return heading
     material_headings = [pretty_headings(heading) for heading in material_headings]
+    return material_headings
 
-    # Loop through suggested material headings and ask the user if they want to change it
-
-    for i, heading in enumerate(material_headings):
-        # print(heading)
-        if use_defaults:
-            user_input=heading
-        else:
-            user_input = input(f"Suggested heading: {heading}\nPress enter to keep the same, or write the desired name to change:")
-        if user_input != '':
-            material_headings[i] = user_input
-
-    print(material_headings)
-
+def convert_files_totext(filenames):
     # Extract text from the files
     # Supported file formats: https://textract.readthedocs.io/en/stable/ + MarkDown
     texts = []
@@ -81,13 +45,13 @@ def main():
                 f.close()
 
         texts.append(text)
+    return texts
 
+def create_chunck_dataframe(material_headings, texts):
     # Create data frame
-
     df = pd.DataFrame({'Heading': material_headings, 'Text': texts})
 
     # Create chunks
-
     from langchain.text_splitter import CharacterTextSplitter
     text_splitter = CharacterTextSplitter(        
         separator = "\n\n",
@@ -96,24 +60,62 @@ def main():
         length_function = len,
         is_separator_regex = False,
     )
-
     df['Text_Splitted'] = df['Text'].apply(text_splitter.split_text)
-
-
     # Append Heading to the top of chunk
-
     df['Text_Splitted_w_Headings'] = df.apply(lambda row: ["Source: " + row['Heading'] + '\n' + chunk for chunk in row['Text_Splitted']], axis=1)
+    return df
 
-    # Create master chunk
-
+def create_vector_store(df,store_type="faiss"):
     master_chunk = []
     for i, row in df.iterrows():
         master_chunk += row['Text_Splitted_w_Headings']
 
     # Create vector store
-
     embeddings = HuggingFaceInstructEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    vector_store = FAISS.from_texts(master_chunk, embeddings)
+    if store_type=="faiss":
+        vector_store = FAISS.from_texts(master_chunk, embeddings)
+    else:
+        print("Unsupported vector store detected. Returning None.")
+        return None
+    return vector_store
+
+def main():
+
+    # Scan files from the course_material directory (or alternative)
+    material_directory=args.load_dir+"/"
+    folder = args.save_dir+"/"
+    use_defaults = args.use_defaults
+    print("Running script with the following arguments:")
+    print(f"Directory for the course materials: {material_directory}")
+    print(f"Directory to save the vector store: {folder}")
+    print(f"Run the script with sensible defaults: {use_defaults}")
+    print("---")
+
+    print("The following files will be processed:\n")
+    if use_defaults:
+        dir_input = material_directory
+    else:
+        dir_input = input(f"Default materials directory: {material_directory}\nPress enter to keep the same, or write the desired name to change: ")
+    if dir_input != '':
+        material_directory = dir_input
+    
+    filenames=read_filenames_from_directory(material_directory=material_directory)
+    print(filenames)
+    material_headings=create_material_headings_from_filenames(filenames,material_directory)
+    # Loop through suggested material headings and ask the user if they want to change it
+    for i, heading in enumerate(material_headings):
+        # print(heading)
+        if use_defaults:
+            user_input=heading
+        else:
+            user_input = input(f"Suggested heading: {heading}\nPress enter to keep the same, or write the desired name to change:")
+        if user_input != '':
+            material_headings[i] = user_input
+    print(material_headings)
+
+    texts = convert_files_totext(filenames)    
+    df = create_chunck_dataframe(material_headings, texts)
+    vector_store = create_vector_store(df,store_type="faiss")
 
     # Try querying the vector store
 
@@ -176,8 +178,15 @@ def main():
         print("Success!")
     except Exception as e:
         print(f"An error occurred while loading vector store from disk: {e}")
+    
+    return df
 
 if __name__ == "__main__":
-    main()
+    parser=argparse.ArgumentParser(description="Read course materials and save to vector store.")
+    parser.add_argument('-d','--load_dir',help="Directory for the course materials. ",required=False, default="course_material")
+    parser.add_argument('-s','--save_dir',help="Directory to save the vector store.",required=False, default="course_material_vdb")
+    parser.add_argument('-u','--use_defaults',action='store_true', help="Run the script with sensible defaults.",required=False)
+    args = parser.parse_args()
+    df = main()
 
 
