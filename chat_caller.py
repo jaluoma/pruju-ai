4bd06a18d6db2d0455b41eded9f17a7261a41d15
Jukka Luoma <jukka.luoma@aalto.fi>
2023-12-12 22:24:59 +0200

    Fix bug related to custom header missing in azure

--- chat_caller.py
+++ chat_caller.py
@@ -49,22 +49,21 @@ def initialize_chat(llm_provider):
             if os.getenv("AZURE_OPENAI_CUSTOM_BACKEND") is not None:
                 if request.url.path == "/chat/completions":
                     request.url = request.url.copy_with(path=os.getenv("AZURE_OPENAI_CUSTOM_BACKEND"))
-                
         chat = ChatOpenAI(
             openai_api_base=os.getenv("MODEL_ENDPOINT"),
             model_name=default_model,
             temperature=0,
-            default_headers = {
-                os.getenv("AZURE_OPENAI_CUSTOM_HEADER"): os.environ.get("OPENAI_API_KEY"),
-            },
+            default_headers={"Authorization": 
+                             "Bearer "+os.environ.get("OPENAI_API_KEY")
+                            },
             http_client=httpx.Client(
                 event_hooks={
                     "request": [update_base_url],
                 }
                 ),
             )
-        if os.getenv("AZURE_OPENAI_CUSTOM_HEADER") is None:
-            chat.default_headers={"Authorization": "Bearer "+os.environ.get("OPENAI_API_KEY")}
+        if os.getenv("AZURE_OPENAI_CUSTOM_HEADER") is not None:
+            chat.default_headers={os.getenv("AZURE_OPENAI_CUSTOM_HEADER"): os.environ.get("OPENAI_API_KEY")}
     elif llm_provider=="openai":
         chat = ChatOpenAI(temperature=0,
                         model_name=default_model)
@@ -74,6 +73,7 @@ def initialize_chat(llm_provider):
         chat = None
     else:
         raise ValueError("LLM-provider not recognized. Check LLM_PROVIDER environment variable.")
+    print(chat)
     return chat, default_model
 
 try:
